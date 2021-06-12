0608392e7aa90d3f64cf862b01e5e3738eb977f0
Danny <ctct25@naver.com>
2021-06-12 19:52:10 +0900

    21/06/10 객체지향 Practice08 예외,기본API문제

--- src/com/javaex/ex03/PointApp.java
+++ src/com/javaex/ex03/PointApp.java
@@ -8,6 +8,11 @@ public class PointApp {
 		
 		System.out.println(p.toString());
 
+		//Point.java에 toString이 없고 App에서 toString을 출력하라한다면
+		//extends 되어있진 않지만 Object인 단군의 toString의 값이 출력이 
+		//되는것이다. 
+		
+		
 	}
 
 }
